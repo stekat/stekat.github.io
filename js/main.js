239c40c07a2bffc061aa5a5acfc3205aca668e15
stekat <stefandietsch@gmail.com>
2019-12-27 07:10:34 +0100

    Javascript verbessert.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 function insertContact() {
     var c = ['l','a','@','n','h','o','s','.','t','r','e','k','v']
-    var hostnames = window.location.hostname.split('.').reverse();
-    var textMail = c[4]+c[1]+c[0]+c[0]+c[5]+c[2]+hostnames[1];
+    
+    var hostnameRegexMatch = window.location.hostname.match(/.*?\.(.*)/);
+
+    if (hostnameRegexMatch === null){
+      return;
+    }
+  
+    var textMail = c[4]+c[1]+c[0]+c[0]+c[5]+c[2]+hostnameRegexMatch[1];
     var textXmpp = c[6]+c[8]+c[10]+c[11]+c[1]+c[8]+c[2]+c[8]+c[9]+c[1]+c[6]+c[4]+c[6]+c[10]+c[9]+c[12]+c[10]+c[9]+c[7]+c[3]+c[10]+c[8];
     document.getElementById("print-mail").innerHTML = textMail;
     document.getElementById("print-xmpp").innerHTML = textXmpp;
