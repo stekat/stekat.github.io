a6d9473a4f1372f848bf02798a8f21b48e08f58c
stekat <stefandietsch@gmail.com>
2019-12-27 06:34:24 +0100

    Fehler behoben

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function insertContact() {
     var c = ['l','a','@','n','h','o','s','.','t','r','e','k','v']
-    var textMail = c[4]+c[1]+c[0]+c[0]+c[5]+c[2]+window.location.hostname;
+    var hostnames = window.location.hostname.split('.').reverse();
+    var textMail = c[4]+c[1]+c[0]+c[0]+c[5]+c[2]+hostnames[1];
     var textXmpp = c[6]+c[8]+c[10]+c[11]+c[1]+c[8]+c[2]+c[8]+c[9]+c[1]+c[6]+c[4]+c[6]+c[10]+c[9]+c[12]+c[10]+c[9]+c[7]+c[3]+c[10]+c[8];
     document.getElementById("print-mail").innerHTML = textMail;
     document.getElementById("print-xmpp").innerHTML = textXmpp;
