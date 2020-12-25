8c6df6ba7be54cc0f7d51c40f331e0db84194f66
stekat <stefandietsch@gmail.com>
2020-12-25 23:05:36 +0100

    Umstellung auf [matrix]

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function insertContact() {
-    var c = ['l','a','@','n','h','o','s','.','t','r','e','k','v']
-    
+    var c = ['l','a','@','n','h','o','s','.','t','r','e','k','v',':','m','i','x','g'];
+  
     var hostnameRegexMatch = window.location.hostname.match(/.*?\.(.*)/);
 
     if (hostnameRegexMatch === null){
@@ -8,11 +8,11 @@ function insertContact() {
     }
   
     var textMail = c[4]+c[1]+c[0]+c[0]+c[5]+c[2]+hostnameRegexMatch[1];
-    var textXmpp = c[6]+c[8]+c[10]+c[11]+c[1]+c[8]+c[2]+c[8]+c[9]+c[1]+c[6]+c[4]+c[6]+c[10]+c[9]+c[12]+c[10]+c[9]+c[7]+c[3]+c[10]+c[8];
+    var textMatrix = c[2]+c[6]+c[8]+c[10]+c[11]+c[1]+c[8]+c[13]+c[14]+c[1]+c[8]+c[9]+c[15]+c[16]+c[7]+c[5]+c[9]+c[17];
     document.getElementById("print-mail").innerHTML = textMail;
-    document.getElementById("print-xmpp").innerHTML = textXmpp;
+    document.getElementById("print-matrix").innerHTML = textMatrix;
     document.getElementById("mail").href = "mailto:"+textMail;
-    document.getElementById("xmpp").href = "xmpp:"+textXmpp;
+    document.getElementById("matrix").href = "https://matrix.to/#/"+textMatrix;
   }
   
   window.onload = insertContact;
